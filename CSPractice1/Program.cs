1212c4155875bbcf50d5979d3fc9eee902395c0a
Hanrabbit06 <suye8794@gmail.com>
2024-07-01 14:03:30 +0900

    #5 22=8. 속성(Property)

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class box
+    {
+        public int width { get; set; }
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set {
+                if (value < 0)
+                {
+                    height = value;
+                }
+                else { throw new Exception(); }
+            }
+        }
+
+
+    }
     class Sample
     {
         public static int value;
@@ -231,6 +250,12 @@ namespace CSPractice1
             Console.WriteLine("세 번째 위치");
             
             //#5 22-7. 소멸자
+
+            //#5 22=8. 속성(Property)
+            box b = new box();
+            b.width = 100;
+            b.Height = 100;
+            //b.Height = -100; //예외발생
 
 
         }
